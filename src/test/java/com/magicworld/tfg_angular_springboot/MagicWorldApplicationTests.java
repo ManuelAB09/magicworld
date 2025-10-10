cec10a8f2770411cf3bd5e3a9c150bafa038fce2
Manuel Artero <manuel.artero04@gmail.com>
2025-10-10 09:56:15 +0200

    feat: Third try of codacy CI

--- src/test/java/com/magicworld/tfg_angular_springboot/MagicWorldApplicationTests.java
+++ src/test/java/com/magicworld/tfg_angular_springboot/MagicWorldApplicationTests.java
@@ -2,8 +2,10 @@ package com.magicworld.tfg_angular_springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MagicWorldApplicationTests {
 
 	@Test
