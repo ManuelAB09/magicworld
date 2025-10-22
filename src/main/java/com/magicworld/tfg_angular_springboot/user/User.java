8a07ef8466f3561b575742d9c45a9cb70db924f1
Manuelgithuv <manuel.artero04@gmail.com>
2025-10-22 10:48:47 +0200

    fix: Commit pre conflict

--- src/main/java/com/magicworld/tfg_angular_springboot/user/User.java
+++ src/main/java/com/magicworld/tfg_angular_springboot/user/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User extends BaseEntity implements UserDetails {
 
     @Column(unique = true, nullable = false)
